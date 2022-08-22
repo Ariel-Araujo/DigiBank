086cf3893ea829b6316413f631048874b778e3d0
Ariel Araujo <a.lucasdearaujo@outlook.com>
2022-08-22 13:31:52 -0300

    DigiBank

---
+++ Classes/ContaCorrente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiBank.Classes
+{
+    public class ContaCorrente : Conta
+    {
+        public ContaCorrente()
+        {
+            this.NumeroConta = "00" + Conta.NumeroDaContaSequencial;
+        }
+
+    }
+}
---
+++ Contratos/IConta.cs
@@ -0,0 +1,22 @@
+﻿using DigiBank.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiBank.Contratos
+{
+    public interface IConta
+    {
+        void Deposita(double valor);
+        bool Saca(double valor);
+        double ConsultaSaldo();
+        string GetCodigoDobanco();
+        string GetNumeroAgencia();
+        string GetNumeroConta();
+        List<Extrato> Extrato();
+
+    }
+}
